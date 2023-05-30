e3ca74f640fea39fae9b29dd79d100fdd5cb81a7
Ashok Patel <96565022+ashokpatel574@users.noreply.github.com>
2023-05-30 22:09:57 +0530

    Initial commit and added Routes in separate file

---
+++ src/frontend/routes/AppRouter.js
@@ -0,0 +1,122 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// components
+import MockAPI from "../components/mockApi/MockApi";
+import RequireAuth from "../components/requiresAuth/RequireAuth";
+import App from "../../App";
+
+// context
+import DataContextProvider from "../context/DataContext";
+import { AuthContextProvider } from "../context/AuthContext";
+
+// public pages
+import Home from "../pages/Home";
+import ProductListingPage from "../pages/ProductListingPage";
+import SingleProductPage from "../pages/SingleProductPage";
+import CartPage from "../pages/CartPage";
+import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
+import ErrorPage from "../pages/ErrorPage";
+
+// private pages
+import WishListPage from "../pages/WishListPage";
+import CheckoutPage from "../pages/CheckoutPage";
+import ProfilePage from "../pages/ProfilePage";
+import UserProfile from "../components/profile/UserProfile";
+import UserAddress from "../components/profile/UserAddress";
+
+// React Router version > 6.4
+export const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <AuthContextProvider>
+        <DataContextProvider>
+          <App />
+        </DataContextProvider>
+      </AuthContextProvider>
+    ),
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "products", element: <ProductListingPage /> },
+      { path: "products/:productId", element: <SingleProductPage /> },
+      { path: "cart", element: <CartPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signUp", element: <SignUpPage /> },
+      {
+        path: "wishlist",
+        element: (
+          <RequireAuth>
+            <WishListPage />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "checkout",
+        element: (
+          <RequireAuth>
+            <CheckoutPage />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <RequireAuth>
+            <ProfilePage />
+          </RequireAuth>
+        ),
+        children: [
+          { index: true, path: "userInfo", element: <UserProfile /> },
+          { path: "userAddress", element: <UserAddress /> },
+        ],
+      },
+    ],
+  },
+  { path: "/mock-man", element: <MockAPI /> },
+]);
+
+// React Router version < 6.4
+// const AppRoutes = () => {
+//   return (
+//     <Routes>
+//       <Route path="/mock-man" element={<MockAPI />} />
+//       <Route path="/" element={<Home />} />
+//       <Route path="/products" element={<ProductListingPage />} />
+//       <Route path="/products/:productId" element={<SingleProductPage />} />
+//       <Route
+//         path="/wishlist"
+//         element={
+//           <RequireAuth>
+//             <WishListPage />
+//           </RequireAuth>
+//         }
+//       />
+//       <Route path="/cart" element={<CartPage />} />
+//       <Route
+//         path="/checkout"
+//         element={
+//           <RequireAuth>
+//             <CheckoutPage />
+//           </RequireAuth>
+//         }
+//       />
+//       <Route
+//         path="/profile"
+//         element={
+//           <RequireAuth>
+//             <ProfilePage />
+//           </RequireAuth>
+//         }
+//       >
+//         <Route index path="userInfo" element={<UserProfile />} />
+//         <Route path="userAddress" element={<UserAddress />} />
+//       </Route>
+//       <Route path="/login" element={<LoginPage />} />
+//       <Route path="/signUp" element={<SignUpPage />} />
+
+//       <Route path="*" element={<ErrorPage />} />
+//     </Routes>
+//   );
+// };
